ea57a3b23d437aeed95bc09989f3ce300d395e17
Tracy Atteberry <tracy@magicbydesign.com>
2022-12-03 00:43:20 -0600

    Add new files

--- 2022/03/solution.rb
+++ 2022/03/solution.rb
@@ -2,17 +2,21 @@
 
 input = File.readlines('input.txt', chomp: true)
 
+priority = ->(letter) { ('a'..'z').include?(letter) ? letter.ord - 96 : letter.ord - 38 }
+
 p1 = input
   .map { |line| line.chars.each_slice(line.size / 2).map(&:join) }
-  .map { |pair| (pair.first.chars & pair.last.chars).join }
-  .map { |letter| ('a'..'z').include?(letter) ? letter.ord - 96 : letter.ord - 38 }
+  .map { |pair| pair.map(&:chars).inject(&:&) }
+  # .map { |pair| (pair.first.chars & pair.last.chars).join }
+  .map { |letter| priority.call(letter) }
   .sum
 
 puts p1
 
 p2 = input.each_slice(3).map do |group|
-    group.map(&:chars).inject(&:&)
-    .map { |letter| ('a'..'z').include?(letter) ? letter.ord - 96 : letter.ord - 38 }
+  group
+    .map(&:chars).inject(&:&)
+    .map { |letter| priority.call(letter) }
     .first
 end.sum
 
