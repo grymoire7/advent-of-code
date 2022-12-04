6fedead87e0aa768fc41d295694e9dbb0f60cef6
Tracy Atteberry <tracy@magicbydesign.com>
2022-12-03 23:53:12 -0600

    Add day 04

---
+++ 2022/04/solution.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+input = File.readlines('input.txt', chomp: true)
+input.map! do |line|
+  a, b, c, d = line.split(/[,-]/).map(&:to_i)
+  [ (a..b), (c..d) ]
+end
+
+# Part 1
+sum = input.map { |pair| a, b = pair; (a.cover?(b) || b.cover?(a)) ? 1 : 0 }.sum
+p sum
+
+# Part 2
+sum = input.map { |pair| a, b = pair; (a.cover?(b.first) || b.cover?(a.first)) ? 1 : 0 }.sum
+p sum
+
